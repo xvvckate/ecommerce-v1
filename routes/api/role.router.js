8f981fd57ca7d5ec4bacb66305c0652477904e58
katejeo <kateyohannes@gmail.com>
2023-01-03 02:13:12 -0800

    role moddified

--- routes/api/role.router.js
+++ routes/api/role.router.js
@@ -1,13 +1,13 @@
 
-const express = require("express")
-const router = express.Router()
+// const express = require("express")
+// const router = express.Router()
 
 
-const { ROLES } = require("../../config/roleList")
-const verifyRoles = require("../../middleware/verifyRoles")
-const roleController = require("../../controllers/roleController")
+// const { ROLES } = require("../../config/roleList")
+// const verifyRoles = require("../../middleware/verifyRoles")
+// const roleController = require("../../controllers/roleController")
 
-router.route("/grant").post(roleController.grantRole)
-router.route("/revock").post(verifyRoles(ROLES.ADMIN), roleController.revockRole)
+// router.route("/grant").post(roleController.grantRole)
+// router.route("/revock").post(verifyRoles(ROLES.ADMIN), roleController.revockRole)
 
-module.exports = router
+// module.exports = router
