415210f8588d989030b55b4a07be1fafa8ae1da7
katejeo <kateyohannes@gmail.com>
2023-01-03 00:45:23 -0800

    transction model completed

---
+++ models/transaction.model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const TransactionSchema = new Schema({
+
+    order_reference : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : "order",
+        required : true
+    }],
+    item_total_price : { tpye: Number, required : true },
+    bicker_price : { type : Number, required : true },
+    total_price : { type : Number, required : true},
+    recived_by : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "user",
+    },
+    is_complete : { type : Boolean, default : false }
+},{
+    timestamps : true
+})
+
+const Transaction = mongoose.model("transaction", TransactionSchema)
+module.exports = Transaction
