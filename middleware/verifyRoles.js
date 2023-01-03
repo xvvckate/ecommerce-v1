7f9d1103ea0e97ec1d59038e3577424cc45f8479
katejeo <kateyohannes@gmail.com>
2023-01-03 10:11:19 -0800

    auth modified

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,6 +1,13 @@
 const errors = require("http-errors")
 const verifyRoles = (...allowedRoles)=>{
-    // pass
+    return (req, res, next)=>{
+        if(!req?.roles) throw errors.Unauthorized()
+        const rolesArray = [ ...allowedRoles ]
+
+        const result = req.roles.map(role=> rolesArray.includes(role)).find(value=> value == true)
+        if(!result) throw errors.Unauthorized()
+        next()
+    }
 }
 
 module.exports = verifyRoles
