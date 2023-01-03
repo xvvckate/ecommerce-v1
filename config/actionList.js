be7190db0ffa69f2a4ee41e17d66b2272d5e52ef
katejeo <kateyohannes@gmail.com>
2023-01-03 00:45:43 -0800

    action list

---
+++ config/actionList.js
@@ -0,0 +1,14 @@
+const ACTIONS = {
+    "CANCLED" : "CANCLED",
+    "IN_PROGRESS" : "IN_PROGRESS",
+    "DELIVERED" : "DELIVERED",
+    "OUT_OF_STOCK" : "OUT_OF_STOCK",
+
+    "PENDING" : "PENDING",
+    "IN_ITS_WAY" : "IN_ITS_WAY",
+
+}
+
+
+
+module.exports = { ACTIONS }
