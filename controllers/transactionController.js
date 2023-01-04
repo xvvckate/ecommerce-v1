3fc5eb89f61eca3938f8385c3c4344210680f361
katejeo <kateyohannes@gmail.com>
2023-01-03 23:21:32 -0800

    transaction controller modified

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -4,8 +4,10 @@ const { commitTransaction } = require("../helper/transaction.helper")
 const Transaction = require("../models/transaction.model")
 
 const getTransaction = async (req, res, next)=>{
+    const { id } = req.params
     try{
-
+        const data = await Transaction.findById(id).populate("order_by", "-_id profile.fullname").populate('order_reference', "-_id quantity order_status").exec()
+        res.status(200).json(data)
     }catch(err){
         next(err)
     }
@@ -13,7 +15,7 @@ const getTransaction = async (req, res, next)=>{
 
 const getTransactions = async (req, res, next)=>{
     try{
-        const data = await Transaction.find()
+        const data = await Transaction.find().populate("order_by", "-_id profile.fullname").populate('order_reference', "-_id quantity order_status").exec()
         res.status(200).json(data)
     }catch(err){
         next(err)
@@ -22,10 +24,13 @@ const getTransactions = async (req, res, next)=>{
 
 const createTransaction = async (req, res, next)=>{
     try{
-        await commitTransaction()
+        const doc = await commitTransaction(req.data._id)
+        console.log(doc)
+        const data = await Transaction.create(doc)
         res.status(201).json({
             status : true,
-            message : "transaction created"
+            message : "transaction created",
+            data
         })
     }catch(err){
         next(err)
@@ -33,7 +38,7 @@ const createTransaction = async (req, res, next)=>{
 }
 
 module.exports = { 
-    // getTransaction, 
+    getTransaction, 
     getTransactions, 
     createTransaction
 }
