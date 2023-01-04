b91a4d8f96f64e0334ec15124c2e8a10e0e9e407
katejeo <kateyohannes@gmail.com>
2023-01-04 00:44:40 -0800

    checking\ strictPopulate

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose")
 const connectDB = async ()=>{
     try{
         
-        mongoose.set('strictQuery', true);
+        mongoose.set("strictQuery", true);
+        // mongoose.set("strictPopulate", false)
         await mongoose.connect(process.env.DB_URI, {
             useUnifiedTopology : true,
             useNewUrlParser : true
