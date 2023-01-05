fac3abf50de9f89b30c7e18eadbd3726b452d978
katejeo <kateyohanne@gmail.com>
2023-01-05 08:43:47 -0800

    getTransaction route populate updated

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -6,8 +6,36 @@ const Transaction = require("../models/transaction.model")
 const getTransaction = async (req, res, next)=>{
     const { id } = req.params
     try{
-        const data = await Transaction.findById(id).populate("order_by", "-_id profile.fullname").populate('order_reference', "-_id quantity order_status").exec()
+        const doc = await Transaction.findById(id).populate({ path : "order_by", select : "-_id profile.fullname"}).populate({path : 'order_reference', populate : { path : "item", populate : { path : "item", select:"-_id item brand", populate : { path : "brand"}}}}).exec()
+        const { _id, order_by,item_total_price, total_price, bicker_price, order_reference } = doc
+        const { profile } = order_by
+        const { firstname, lastname } = profile.fullname     
+        const data = {
+            transaction_reference : _id,
+            order_by : `${firstname} ${lastname}`,
+            numeber_of_orders : order_reference.length,
+            orders : order_reference.map(order=>{
+                const { price, color, size, item } = order.item
+                
+                return {
+                    item_name : item?.item,
+                    brand_nam : item?.brand.brand,
+                    color,
+                    size,
+                    quantity : order.quantity,
+                    fixed_price : price.fixPrice,
+                    discount_percent : `${price.discountPercent}%`
+                }
+
+            }),
+            bicker_price,
+            total_price, 
+            item_total_price,
+        }
+
+        console.log("apple")
         res.status(200).json(data)
+        
     }catch(err){
         next(err)
     }
