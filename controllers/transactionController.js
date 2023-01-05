0413690550507f67a3cf4d0ef61b4e56ff92a49c
katejeo <kateyohannes@gmail.com>
2023-01-05 10:38:50 -0800

    transaction population done!

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -7,33 +7,32 @@ const getTransaction = async (req, res, next)=>{
     const { id } = req.params
     try{
         const doc = await Transaction.findById(id).populate({ path : "order_by", select : "-_id profile.fullname"}).populate({path : 'order_reference', populate : { path : "item", populate : { path : "item", select:"-_id item brand", populate : { path : "brand"}}}}).exec()
-        const { _id, order_by,item_total_price, total_price, bicker_price, order_reference } = doc
+        const { _id, order_by, item_total_price, total_price, bicker_price, order_reference } = doc
         const { profile } = order_by
-        const { firstname, lastname } = profile.fullname     
+        const { firstname, middlename, lastname } = profile?.fullname     
         const data = {
             transaction_reference : _id,
-            order_by : `${firstname} ${lastname}`,
+            order_by : `${firstname} ${middlename} ${lastname}`,
             numeber_of_orders : order_reference.length,
             orders : order_reference.map(order=>{
-                const { price, color, size, item } = order.item
+                const { price, color, size, item } = order?.item
                 
                 return {
                     item_name : item?.item,
                     brand_nam : item?.brand.brand,
                     color,
                     size,
-                    quantity : order.quantity,
-                    fixed_price : price.fixPrice,
-                    discount_percent : `${price.discountPercent}%`
+                    quantity : order?.quantity,
+                    fixed_price : price?.fixPrice,
+                    discount_percent : `${price?.discountPercent}%`
                 }
 
             }),
+            item_total_price,
             bicker_price,
             total_price, 
-            item_total_price,
         }
 
-        console.log("apple")
         res.status(200).json(data)
         
     }catch(err){
@@ -43,7 +42,34 @@ const getTransaction = async (req, res, next)=>{
 
 const getTransactions = async (req, res, next)=>{
     try{
-        const data = await Transaction.find().populate("order_by", "-_id profile.fullname").populate('order_reference', "-_id quantity order_status").exec()
+        const docs = await Transaction.find().populate({ path : "order_by", select : "-_id profile.fullname"}).populate({path : 'order_reference', populate : { path : "item", populate : { path : "item", select:"-_id item brand", populate : { path : "brand"}}}}).exec()
+        const data = docs.map(doc=>{
+            const { _id, order_by, item_total_price, total_price, bicker_price, order_reference } = doc
+            const { profile } = order_by
+            const { firstname, middlename, lastname } = profile?.fullname     
+            return {
+                transaction_reference : _id,
+                order_by : `${firstname} ${middlename} ${lastname}`,
+                numeber_of_orders : order_reference.length,
+                orders : order_reference.map(order=>{
+                    const { price, color, size, item } = order?.item
+                    
+                    return {
+                        item_name : item?.item,
+                        brand_nam : item?.brand.brand,
+                        color,
+                        size,
+                        quantity : order?.quantity,
+                        fixed_price : price?.fixPrice,
+                        discount_percent : `${price?.discountPercent}%`
+                    }
+
+                }),
+                item_total_price,
+                bicker_price,
+                total_price, 
+            }
+        })
         res.status(200).json(data)
     }catch(err){
         next(err)
