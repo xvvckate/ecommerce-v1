62a7fdc5e01b89fee7d3c02fd7c778ee98673049
katejeo <kateyohannes@gmail.com>
2023-01-05 13:05:09 -0800

    getOrder population completed

--- controllers/deliveryController.js
+++ controllers/deliveryController.js
@@ -2,16 +2,40 @@ const Delivery = require("../models/delivery.model")
 const errors = require("http-errors")
 const { deliverySchema } = require("../validation/delivery.validation")
 
+
 const getDelivery = async (req, res, next)=>{
     const { id } = req.params
     if(!id) return errors.BadRequest()
 
     try{
-        const data = await Delivery.findById(id).populate({
-            path : "transaction_reference", select: "-_id item_total_price bicker_price total_price", populate : { 
-                path : "order_by", select:"-_id profile.fullname"
+        const doc = await Delivery.findById(id).populate({
+            path : "transaction_reference", populate : { 
+                path : "order_by", select:"-_id profile total_price is_complete" 
             }
         }).exec()
+        
+        const { transaction_reference, delivery_address, delivery_status } = doc
+        const { order_by, item_total_price, bicker_price, total_price, is_complete } = transaction_reference
+        const { profile } = order_by
+        const { firstname, middlename, lastname } = profile?.fullname
+        
+        const _address = profile.address.find(address=> address._id !== delivery_address)
+        const address = {
+            country : _address.country,
+            street : _address.street,
+            city : _address.city,
+        }
+        
+        const data = { 
+            delivery_reference : doc._id,
+            order_by : `${firstname} ${middlename} ${lastname}`,
+            delivery_address : address,
+            item_total_price,
+            bicker_price,
+            total_price,
+            is_complete,
+            delivery_status           
+        }
 
         res.status(200).json(data)
     }catch(err){
