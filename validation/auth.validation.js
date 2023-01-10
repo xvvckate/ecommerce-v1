8eb51b5351a88cabe593f343ddb570b833faa510
katejeo <kateyohannes@gmail.com>
2023-01-09 23:59:55 -0800

    validation updated

--- validation/auth.validation.js
+++ validation/auth.validation.js
@@ -1,15 +1,15 @@
 const Joi = require("joi")
 
 const authenticateSchema = Joi.object({
-    username : Joi.string()
-        .lowercase()
-        .min(5)
-        .max(20)
-        .required(),
     phone_number : Joi.string()
         .min(6)
         .max(30)
-        .regex(/[0-9]{3,30}/)
+        .regex(/[a-zA-Z0-9]{3,30}/)
+        .required(),
+    password : Joi.string()
+        .min(6)
+        .max(30)
+        .regex(/[a-zA-Z0-9]{3,30}/)
         .required()
 })
 
