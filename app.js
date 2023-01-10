9d948e6345fdfbf1eaaf42c630ef0239ba44820c
katejeo <kateyohannes@gmail.com>
2023-01-10 00:01:42 -0800

    app.js updated

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan("dev"))
 
+app.use("/", require("./routes/api/public.route"))
 app.use("/auth", require("./routes/api/auth.router"))
 app.use(verifyAuth)
 
