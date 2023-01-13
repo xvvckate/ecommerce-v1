1d23638f318bf5028d1267cf442f7a440af534ad
katejeo <kateyohannes@gmail.com>
2023-01-12 22:13:19 -0800

    remove password field and modife phone_number validation field

--- validation/auth.validation.js
+++ validation/auth.validation.js
@@ -2,14 +2,8 @@ const Joi = require("joi")
 
 const authenticateSchema = Joi.object({
     phone_number : Joi.string()
-        .min(6)
-        .max(30)
-        .regex(/[a-zA-Z0-9]{3,30}/)
-        .required(),
-    password : Joi.string()
-        .min(6)
-        .max(30)
-        .regex(/[a-zA-Z0-9]{3,30}/)
+        .length(9)
+        .regex(/^[0-9]/)
         .required()
 })
 
