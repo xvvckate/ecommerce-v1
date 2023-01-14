56985c07345adef80c4401b4edf4bc9d7b059e41
katejeo <kateyohannes@gmail.com>
2023-01-13 21:40:43 -0800

    get item detail added to public router

--- routes/api/public.route.js
+++ routes/api/public.route.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
+const itemDetailController = require("../../controllers/itemDetailController")
 const userController = require("../../controllers/userController")
 
-
+router.get('/products', itemDetailController.getItemDetails)
 router.post("/register", userController.registerUser)
 
 module.exports = router
